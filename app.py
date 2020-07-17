5e8977e964926cf02e30b75f392a22ed48f03a67
cjm322 <conor.murphy@gmail.com>
2020-07-17 17:34:27 -0400

    added yfinance

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from Nasdaqomx import Nasdaqomx
+import yfinance as yf
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
 ndx = Nasdaqomx()
 index = 'EMCLOUD'
 
 tickers = ndx.fetchTickers(index)
 
-print(tickers)
+current_date = datetime.today()
+lookback_date = current_date - relativedelta(months=6)
+
+stocks = yf.download(tickers, lookback_date, current_date)
